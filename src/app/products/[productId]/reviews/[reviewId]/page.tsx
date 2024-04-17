e7be63621b17e289188770288309c44f4cad7709
Renzaki <renzakithakuri@gmail.com>
2024-04-17 08:54:31 +0545

    creating custom not-found page

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,8 +1,14 @@
+import notFound from "@/app/not-found";
+import NotFound from "./not-found";
+
 export default function reviewDetail({
   params,
 }: {
   params: { productId: string; reviewId: string };
 }) {
+  if(parseInt(params.reviewId)>1000){
+   return NotFound()
+  }
   return (
     <>
       <h1>Review {params.reviewId} for product {params.productId}</h1>

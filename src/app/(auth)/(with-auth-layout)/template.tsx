6847b5443e8491da6b6262b56760c38736444fc0
Renzaki <renzakithakuri@gmail.com>
2024-04-17 15:27:50 +0545

    templates part

--- src/app/(auth)/(with-auth-layout)/template.tsx
+++ src/app/(auth)/(with-auth-layout)/template.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 export default function AuthLayout({
   children,
@@ -13,12 +14,27 @@ export default function AuthLayout({
     { name: "forgot-password", href: "/forgot-password" },
   ];
   const pathname = usePathname();
+  const [input, setInput] = useState("");
 
   return (
     <div className="">
+      <div className="text-black">
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+      </div>
       {navLinks.map((link) => {
         const isActive = pathname.startsWith(link.href);
-        return <Link href={link.href} className={isActive?"font-bold mr-4":"text-blue-500 mr-4"}>{link.name}</Link>;
+        return (
+          <Link
+            href={link.href}
+            className={isActive ? "font-bold mr-4" : "text-blue-500 mr-4"}
+          >
+            {link.name}
+          </Link>
+        );
       })}
       <h2>Inner Layout</h2>
       {children}

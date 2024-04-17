b20c50e0a5ebcde152ba43ba0ec75233d7cbde3c
Renzaki <renzakithakuri@gmail.com>
2024-04-17 21:09:46 +0545

    condtional routing

--- src/app/complex-dashboard/@notifications/page.tsx
+++ src/app/complex-dashboard/@notifications/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Notifications() {
   return (
-    <div className="border p-5 flex w-[330px] gap-2 flex-roworder ">
+    <div className="border p-5 flex w-[330px] gap-2 flex-row ">
       <h1>Notifications page</h1>
       <Link className="text-blue-500" href={'/complex-dashboard/archivedd'}>Archived</Link>
     </div>
--- src/app/complex-dashboard/layout.tsx
+++ src/app/complex-dashboard/layout.tsx
@@ -3,22 +3,27 @@ export default function DashBoardLayout({
   users,
   revenue,
   notifications,
+  login,
 }: {
   children: React.ReactNode;
   users: React.ReactNode;
   revenue: React.ReactNode;
   notifications: React.ReactNode;
+  login: React.ReactNode;
 }) {
-  return (
-    <>
-      <div className="m-2">{children}</div>
-      <div className="flex my-5 mx-2">
+  const isLoggedIn = true;
+  return isLoggedIn ? (
+    <div className="m-2">
+      {children}
+      <div className="flex my-5 ">
         <div className="flex">
-            <div className="">{users}</div>
-            <div className="">{revenue}</div>
+          <div className="">{users}</div>
+          <div className="">{revenue}</div>
         </div>
         <div className="flex">{notifications}</div>
       </div>
-    </>
+    </div>
+  ) : (
+    login
   );
 }

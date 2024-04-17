5299f4c2ca6ce187dbfc0899e1d2c65c4761a8e9
Renzaki <renzakithakuri@gmail.com>
2024-04-17 15:20:50 +0545

    navigating methods

--- src/app/(auth)/(with-auth-layout)/layout.tsx
+++ src/app/(auth)/(with-auth-layout)/layout.tsx
@@ -1,8 +1,27 @@
-export default function AuthLayout({children}:{children:React.ReactNode}){
-return(
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const navLinks = [
+    { name: "register", href: "/register" },
+    { name: "login", href: "/login" },
+    { name: "forgot-password", href: "/forgot-password" },
+  ];
+  const pathname = usePathname();
+
+  return (
     <div className="">
-        <h2>Inner Layout</h2>
-        {children}
+      {navLinks.map((link) => {
+        const isActive = pathname.startsWith(link.href);
+        return <Link href={link.href} className={isActive?"font-bold mr-4":"text-blue-500 mr-4"}>{link.name}</Link>;
+      })}
+      <h2>Inner Layout</h2>
+      {children}
     </div>
-)
+  );
 }

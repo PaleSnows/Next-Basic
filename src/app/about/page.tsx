4782ee888ee8f76cf3ff915daeec4464af46a44b
Renzaki <renzakithakuri@gmail.com>
2024-04-17 14:48:21 +0545

    metadata and links

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,15 @@
+import Link from "next/link";
+
+export const metadata = {
+  title: "Learning",
+};
+
 export default function About() {
-  return <h1>About Page</h1>;
+  return (
+    <>
+      <h1>About Page</h1>
+      <Link href={'/blog'}>Blog</Link>
+      <Link href={'/products'}>Products</Link>
+    </>
+  );
 }
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,10 +1,17 @@
+import Link from "next/link";
+
 export default function ProductList() {
+  const productId =100
   return (
     <>
       <h1>Product Page</h1>
-      <h2>Product 1</h2>
-      <h2>Product 2</h2>
-      <h2>Product 3</h2>
+      <h2>
+        <Link href={'/products/1'}>Product 1</Link>
+      </h2>
+      <h2> <Link href={'/products/2'}>Product 2</Link></h2>
+      <h2>
+      <Link href={'/products/3'}>Product 3</Link>
+      </h2>
     </>
   );
 }
